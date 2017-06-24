abe4161951ec1dc71bc087db964449c716f04faf
Xin00163 <wangxin00163@yahoo.co.uk>
2017-06-24 23:45:17 +0100

    Basic scraper for English, Chinese product descriptions

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # audit_crawler
+
+## To do
+
+* Create a single file listing all designers
+* For each designer visit url: https://www.net-a-porter.com/us/en/Shop/Designers/Charlotte_Tilbury?pn=1&npp=view_all&image_view=product&dScroll=0
+* For each page want to find product title in English and Chinese as well as pid number
+* For each designer save this to CSV file with headings "pid no", "English", "Chinese"
--- auditList.js
+++ auditList.js
@@ -1,7 +1,55 @@
-$('.description').each(function () {
-  console.log($(this).text().trim().split('\n')[0]);
-});
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var Promise = require("bluebird");
+var app = express();
+var designers = require('./designers');
 
-$('.description a').each(function () {
-  console.log($(this).attr('href').split('/')[2]);
-});
+var parseDesigners = [];
+
+for(var i=0; i<designers.length; i++) {
+  parseDesigners.push(designers[i].split(" ").join("_"))
+}
+
+app.get('/scrape', function (req, res) {
+
+  Promise.map(parseDesigners, function(designer) {
+    var url = "https://www.net-a-porter.com/us/zh/Shop/Designers/" + designer + "?pn=1&npp=view_all&image_view=product&dScroll=0";
+    // var url = "https://www.net-a-porter.com/us/en/Shop/Designers/" + designer + "?pn=1&npp=view_all&image_view=product&dScroll=0";
+    request(url, function (error, response, html) {
+      if (!error) {
+        var $ = cheerio.load(html);
+
+        var products = [];
+
+        $rows = $('.description');
+
+        $rows.each(function (row) {
+          var description = $(this).find('a').attr('title');
+          var pid = $(this).find('a').attr('href').split('/')[2];
+          products.push({
+            'chinese': description,
+            // 'english': description,
+            'pidNo': pid
+          })
+        });
+
+        // var filename = './english/'+ designer + '.json';
+        var filename = './chinese/'+ designer + '.json';
+        fs.writeFile(filename, JSON.stringify(products, null, 4), function (err) {
+          console.log('File successfully written! - Check your project directory for the json files');
+        })
+      }
+    })
+  }).then(function() {
+    console.log("done");
+  });
+
+  res.send('Check your console!');
+
+})
+
+app.listen(3000, function () {
+  console.log('App listening on port 3000')
+})
